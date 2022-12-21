a503c9210495113b7de58bfef78bbc7abdfef167
robertnascimento <robertiver13.03@gmail.com>
2022-12-21 07:24:43 -0300

    first commit

---
+++ core/views.py
@@ -0,0 +1,221 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.models import Permission
+from django.contrib import messages
+from .models import Fornecedor, TipoProduto , Produto, Usuario, Retiradas
+from .forms import FornecedorForm, TipoProdutoForm, ProdutoForm, UsuarioCreationForm, RetiradaForm
+
+
+@login_required
+def home(request):
+    return render(request,'home.html')
+
+
+@login_required
+def perfil(request):
+    return render(request,'perfil.html')
+
+
+
+def registro(request):
+    form = UsuarioCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('login')
+    contexto = {
+        'form': form
+    }
+    
+    return render(request,'registro.html',contexto)
+
+def dados(request,id):
+    user = Usuario.objects.get(pk=id)
+
+    form = UserCreationForm(request.POST or None, instance=user)
+    if form.is_valid():
+        form.save()
+        return redirect('perfil')
+
+    contexto = {
+        'form': form
+    }
+    return render(request, 'dadosEdit.html', contexto)
+
+
+
+"""
+FORNECEDORES
+"""
+@login_required
+def fornecedor_lista(request):
+    fornec = Fornecedor.objects.all()
+    contexto = {
+        'list_fornecedor': fornec
+    }
+    return render(request,'fornecedor_lista.html',contexto)
+
+@login_required
+def fornecedor_cadastro(request):
+    form = FornecedorForm(request.POST or None,request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect('list_fornecedor')
+
+    contexto = {
+        'form': form
+    }
+
+    return render(request,'fornecedorcad.html',contexto)
+
+@login_required
+def fornecedor_Edit(request,id):
+    frnc = Fornecedor.objects.get(pk=id)
+    
+    form = FornecedorForm(request.POST or None,request.FILES or None, instance=frnc)
+    if form.is_valid():
+        form.save()
+        return redirect('list_fornecedor')
+    
+    contexto = {
+        'form': form
+    }
+
+    return render(request, 'fornecedor_edit.html', contexto)
+
+@login_required
+def fornecedor_remover(request, id):
+    frnc = Fornecedor.objects.get(pk=id)
+    frnc.delete()
+    return redirect('list_fornecedores')
+
+
+
+"""
+TIPO PRODUTOS
+"""
+
+@login_required
+def tipoProduto_Lista(request):
+    tpProd = TipoProduto.objects.all()
+    contexto = {
+        'list_tipoProduto': tpProd
+    }
+    return render(request,'tipoprodutolista.html',contexto)
+
+@login_required
+def tipoProduto_Cadastro(request):
+    form = TipoProdutoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('list_tipoProduto')
+
+    contexto = {
+        'form' : form
+    }
+
+    return render(request,'tipoprodutocad.html',contexto)
+
+@login_required
+def tipoProduto_Edit(request,id):
+    tprd = TipoProduto.objects.get(pk=id)
+
+    form = TipoProdutoForm(request.POST or None,instance= tprd)
+    if form.is_valid():
+        form.save()
+        return redirect('list_tipoProduto')
+
+    contexto = {
+        'form' : form
+    }
+
+    return render(request,'tipoprodutoedit.html',contexto)
+
+@login_required
+def tipoProduto_Remover(request,id):
+    tprd = TipoProduto.objects.get(pk=id)
+    tprd.delete()
+    return redirect('list_tipoProduto')
+
+
+
+"""
+PRODUTOS
+"""
+
+@login_required
+def produto_lista(request):
+    prod = Produto.objects.all()
+    contexto = {
+        'list_produto': prod
+    }
+    return render(request,'produto_lista.html',contexto)
+
+@login_required
+def produto_cadastro(request):
+    form = ProdutoForm(request.POST or None,request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect('list_produto')
+
+    contexto = {
+        'form': form
+    }
+    return render(request,'produto_cad.html',contexto)
+
+@login_required
+def produto_edit(request,id):
+    prd = Produto.objects.get(pk=id)
+
+    form = ProdutoForm(request.POST or None,request.FILES or None,instance=prd)
+    if form.is_valid():
+        form.save()
+        return redirect('list_produto')
+
+    contexto = {
+        'form': form
+    }
+    return render(request,'produto_edit.html',contexto)
+
+@login_required
+def produto_remover(request,id):
+    prd = Produto.objects.get(pk=id)
+    prd.delete()
+    return redirect('list_produto')
+
+def retiradas(request,id):
+    prod = Produto.objects.get(pk=id)
+    form = RetiradaForm(request.POST or None)
+
+    if form.is_valid():
+        qtd = request.POST['quantidaderet']
+        qtdInt = int(qtd)
+        if qtdInt <= prod.quantidade:
+            prod.quantidade -= qtdInt
+            messages.success(request,"Retirada realizada com sucesso")
+            form.save()
+            prod.save()
+        else:
+            messages.error(request,"Quantidade solicitada, maior do que à quantidade disponível")
+    
+        return redirect('list_produto')
+
+
+    contexto = {
+        'form': form
+    }
+
+    return render(request,'retirada.html',contexto)
+
+def retiradasrealizadas(request):
+    rtrd = Retiradas.objects.all()
+    contexto = {
+        'rtrok': rtrd
+    }
+    return render(request,'retiradasrealizadas.html',contexto)
+
+def retreal_remover(request, id):
+    rtr = Retiradas.objects.get(pk=id)
+    rtr.delete()
+    messages.error(request,"Retirada apagada")
+    return redirect('rtrok')
---
+++ sgce/urls.py
@@ -0,0 +1,54 @@
+"""sgce URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from core.views import retreal_remover,retiradasrealizadas,retiradas,perfil,dados,fornecedor_lista,fornecedor_Edit,fornecedor_cadastro,fornecedor_remover, tipoProduto_Edit,tipoProduto_Cadastro,tipoProduto_Lista
+from core.views import produto_lista, produto_edit, produto_cadastro, produto_remover, registro,tipoProduto_Remover,home
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('home/',home,name='home'),
+
+    path('perfil/',perfil,name='perfil'),
+    path('login/',LoginView.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(),name='logout'),
+    path('registro/',registro ,name='registro'),
+    path('dados/<int:id>/',dados,name='edit_dados'),
+
+    path('fornecedorCad/',fornecedor_cadastro,name='cad_fornecedor'),
+    path('fornecedorList/',fornecedor_lista,name='list_fornecedor'),
+    path('fornecedorEdit/<int:id>',fornecedor_Edit,name='edit_fornecedor'),
+    path('fornecedorDelete/<int:id>',fornecedor_remover,name='fornecedor_remover'),
+
+    path('tipoProdutoCad/',tipoProduto_Cadastro,name='cad_tipoProduto'),
+    path('tipoProdutoList/',tipoProduto_Lista,name='list_tipoProduto'),
+    path('tipoProdutoEdit/<int:id>',tipoProduto_Edit,name='edit_tipoProduto'),
+    path('tipoProdutoDelete/<int:id>',tipoProduto_Remover,name='del_tipoProduto'),
+
+    path('produtoList/',produto_lista,name='list_produto'),
+    path('produtoCad/',produto_cadastro,name='cad_produto'),
+    path('produtoEdit/<int:id>',produto_edit,name='edit_produto'),
+    path('produtoDelete/<int:id>',produto_remover,name='del_produto'),
+
+    path('retirada/<int:id>/',retiradas,name='retiradas'),
+    path('retiradasrealizadas/',retiradasrealizadas,name='rtrok'),
+    path('retiradasrealizadas/<int:id>',retreal_remover,name='rtrdel'),
+
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
